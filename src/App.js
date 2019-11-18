fa510e9e6765ada6805a624b30d8535df1b0a5a2
allwinjandrews <allwin@labglo.com>
2019-11-18 12:34:46 +0530

    Just Basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Input, Button } from "reactstrap";
 
-function App() {
+export default function App() {
+  const [keyNum, setKeyNum] = useState(0);
+  const [keyAlpha, setKeyAlpha] = useState("a");
+  const [position, setPosition] = useState(0);
+  const [oldNum, setOldNum] = useState([]);
+
+  const current_num = [],
+    new_num = [],
+    alpha = [];
+
+  useEffect(() => {
+    for (var i = 1; i < 27; i++) current_num.push(i);
+    setOldNum(current_num);
+    for (i = 10; i < 36; i++) alpha.push(i.toString(36));
+    console.log(oldNum)
+    console.log(oldNum.indexOf(keyNum))
+    // setPosition(current_num.indexOf(keyNum));
+    // console.log(position);
+  }, [keyNum]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Input
+        onChange={event => setKeyAlpha(event.target.value)}
+        placeholder="Type key alphabet"
+        type="text"
+      />
+      <Input
+        onChange={event => {
+          setKeyNum(event.target.value);
+        }}
+        placeholder="Type key number"
+        type="number"
+      />
+      <Input
+        onChange={event => setKeyNum(event.target.value)}
+        placeholder="Type your code"
+        type="text"
+      />
+      <Button
+        onClick={() => {
+          console.log(keyNum);
+          setPosition(current_num.indexOf(keyNum));
+          console.log(position);
+        }}
+      >
+        encrypt
+      </Button>
+      <div>keyNum={keyNum}</div>
+      <div>position={position}</div>
+    </>
   );
 }
-
-export default App;
