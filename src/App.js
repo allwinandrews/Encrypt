6ed799c65d9f14fcd633cddfa849946d93f8d0a6
allwinjandrews <allwin@labglo.com>
2019-11-19 17:16:14 +0530

    decryption(from numbers to words)

--- src/App.js
+++ src/App.js
@@ -3,31 +3,95 @@ import { Input, Button } from "reactstrap";
 
 export default function App() {
   const [keyNum, setKeyNum] = useState(0);
-  const [keyAlpha, setKeyAlpha] = useState("a");
   const [position, setPosition] = useState(0);
-  const [oldNum, setOldNum] = useState([]);
+  const [curNum, setCurNum] = useState([]);
+  const [newNum, setNewNum] = useState([]);
+  const [alpha, setAlpha] = useState([]);
+  const [code, setCode] = useState("");
+  const [codeDict, setCodeDict] = useState([]);
+  const [final, setFinal] = useState("");
 
-  const current_num = [],
+  var current_num = [],
+    alphas = [],
+    toattach,
+    dict = [],
     new_num = [],
-    alpha = [];
+    words_num,
+    letter_num,
+    words = [];
+
+  const convertToDict = new_num => {
+    for (var i = 0; i <= 25; i++)
+      dict.push({
+        key: new_num[i],
+        value: alpha[i]
+      });
+    return dict;
+  };
+
+  const convertWordsToArrays = letter_num => {
+    var letter = [];
+    for (var i in letter_num) {
+      letter.push(parseInt(letter_num[i]));
+    }
+    return letter;
+  };
+
+  const convertToWordArray = words_num => {
+    for (var i = 0; i < words_num.length; i++) {
+      if (words_num[i].length <= 2) {
+        words.push(parseInt(words_num[i]));
+      } else {
+        letter_num = words_num[i].split(",");
+        letter_num = convertWordsToArrays(letter_num);
+        words.push(letter_num);
+      }
+    }
+    return words;
+  };
+
+  const checkInDict = letter => {
+    for (var i in dict) if (dict[i].key === letter) return dict[i].value;
+  };
+
+  const convertToWords = words_num => {
+    var final = [];
+    for (var i in words_num)
+      if (!Array.isArray(words_num[i]))
+        final.push(" " + checkInDict(words_num[i]) + " ");
+      else {
+        for (var j in words_num[i]) final.push(checkInDict(words_num[i][j]));
+        final.push(" ");
+      }
+    return final.join("");
+  };
+
+  const twistArray = () => {
+    setPosition(curNum.indexOf(keyNum));
+    current_num = curNum;
+    toattach = current_num.splice(0, current_num[curNum.indexOf(keyNum) - 1]);
+    new_num = current_num.concat(toattach);
+    setNewNum(new_num);
+    dict = convertToDict(new_num);
+    setCodeDict(dict);
+    words_num = code.split(" ");
+    words_num = convertToWordArray(words_num);
+    setFinal(convertToWords(words_num));
+  };
+
+  const initialize = () => {
+    for (var i = 1; i < 27; i++) current_num.push(i);
+    setCurNum(current_num);
+    for (i = 10; i < 36; i++) alphas.push(i.toString(36));
+    setAlpha(alphas);
+  };
 
   useEffect(() => {
-    for (var i = 1; i < 27; i++) current_num.push(i);
-    setOldNum(current_num);
-    for (i = 10; i < 36; i++) alpha.push(i.toString(36));
-    console.log(oldNum);
-    console.log(oldNum.indexOf(keyNum));
-    // setPosition(current_num.indexOf(keyNum));
-    // console.log(position);
-  }, [keyNum]);
+    initialize();
+  }, [keyNum, code]);
 
   return (
     <>
-      <Input
-        onChange={event => setKeyAlpha(event.target.value)}
-        placeholder="Type key alphabet"
-        type="text"
-      />
       <Input
         onChange={event => {
           setKeyNum(parseInt(event.target.value));
@@ -36,21 +100,16 @@ export default function App() {
         type="number"
       />
       <Input
-        onChange={event => setKeyNum(event.target.value)}
+        onChange={event => setCode(event.target.value)}
         placeholder="Type your code"
         type="text"
       />
-      <Button
-        onClick={() => {
-          console.log(keyNum);
-          setPosition(current_num.indexOf(keyNum));
-          console.log(position);
-        }}
-      >
-        encrypt
-      </Button>
-      <div>keyNum={keyNum}</div>
-      <div>position={position}</div>
+      {!(keyNum === 0 || code === "") ? (
+        <Button onClick={twistArray}>encrypt</Button>
+      ) : (
+        ""
+      )}
+      <div>{final}</div>
     </>
   );
 }
